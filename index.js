05155eacaab78945b43adf1230361fa19bbf4bd3
Ismael Barroso <mael-sf@hotmail.com>
2021-05-17 20:39:05 -0300

    change chart design

--- index.js
+++ index.js
@@ -3,46 +3,47 @@ const luasCounter = document.getElementById('luas');
 const planetasCounter = document.getElementById('planetas');
 const navesCounter = document.getElementById('naves');
 
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
- async function drawChart() {
-        const response = await swapiGet("vehicles/");
-        const vehiclesArray = response.data.results;
+async function drawChart() {
+    const response = await swapiGet("vehicles/");
+    const vehiclesArray = response.data.results;
 
-        console.log(vehiclesArray)
-        const dataArray = [];
-        dataArray.push(["veiculos","Passageiros"]);
-        vehiclesArray.forEach((vehicle) => {
-            dataArray.push([vehicle.name,Number(vehicle.passengers)])
-        })
-        var data = google.visualization.arrayToDataTable(dataArray);
+    console.log(vehiclesArray)
+    const dataArray = [];
+    dataArray.push(["veiculos", "Passageiros"]);
+    vehiclesArray.forEach((vehicle) => {
+        dataArray.push([vehicle.name, Number(vehicle.passengers)])
+    })
+    var data = google.visualization.arrayToDataTable(dataArray);
 
-        var options = {
-          title: 'My Daily Activities'
-        };
+    var options = {
+        title: 'Maiores Veiculos',
+        legend: 'none'
+    };
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-        chart.draw(data, options);
-      }
+    chart.draw(data, options);
+}
 
 
 preencherCounters();
 preencherTabela();
 
-function preencherCounters(){
+function preencherCounters() {
 
-    Promise.all([swapiGet("people/"), swapiGet("vehicles/"),swapiGet("planets/"),swapiGet("starships/")])
-  .then(function (results){
-      personagensCounter.innerHTML = results[0].data.count;
-      luasCounter.innerHTML = results[1].data.count;
-      planetasCounter.innerHTML = results[2].data.count;
-      navesCounter.innerHTML = results[3].data.count;
-  });
+    Promise.all([swapiGet("people/"), swapiGet("vehicles/"), swapiGet("planets/"), swapiGet("starships/")])
+        .then(function(results) {
+            personagensCounter.innerHTML = results[0].data.count;
+            luasCounter.innerHTML = results[1].data.count;
+            planetasCounter.innerHTML = results[2].data.count;
+            navesCounter.innerHTML = results[3].data.count;
+        });
 }
 
-async function preencherTabela(){
+async function preencherTabela() {
     const response = await swapiGet("films/");
     const tableData = response.data.results;
     tableData.forEach(film => {
@@ -52,10 +53,10 @@ async function preencherTabela(){
         <td>${film.director}</td>
         <td>${film.episode_id}</td>
         </tr>`);
-        
+
     });
 }
 
-function swapiGet(param){
-   return axios.get(`https://swapi.dev/api/${param}`)
+function swapiGet(param) {
+    return axios.get(`https://swapi.dev/api/${param}`)
 }
